85fcf1fb1bc8984ee3232f8c9d14b0df84c6c689
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2023-08-05 18:40:43 +0200

    fix: fix vercel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 //Resources
-app.use(express.static(path.join(__dirname, './public/src')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
