a7843e7e550d09ea72e1c6322c4cb3105bc13306
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2023-08-05 19:08:14 +0200

    fix: remove ?? operator

--- web/src/app/config.js
+++ web/src/app/config.js
@@ -1,7 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 const apiDomain = process.env.API_URL;
-const apiUrl = `${apiDomain ?? ''}/api`;
+const apiUrl = `${apiDomain || ''}/api`;
 
 const config = { isDev, apiUrl };
 
